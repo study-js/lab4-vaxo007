225d52d29fd1f4ec516be6e1101b9a87620cdf3c
Your Name <you@example.com>
2017-11-22 10:37:15 +0400

    l403

---
+++ L403/script.js
@@ -0,0 +1,43 @@
+function calculate(studentsList, studentsName){
+    
+        var differentNmames = [];
+    
+       for(var i=0; i < studentsList.length; i++){
+            if(differentNmames.indexOf(studentsList[i].name) == -1){
+                differentNmames.push(studentsList[i].name);
+            }
+        }
+    
+        return differentNmames;
+    }
+    
+    
+    var students = [
+        {
+            id: 1,
+            name: 'eduard'
+        },
+        {
+         id: 2,
+         name:'Giorgi'
+        },
+        {
+         id: 3,
+         name:'Giorgi'
+         },
+         {
+       
+          id: 4,
+          name: 'Vaxo'
+         },
+         {
+          id: 5,
+          name:'nika'
+         }
+    
+        
+    ];
+    
+    
+    var names = calculate(students);
+    alert (names);
